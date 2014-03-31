1cc7be950f3ae2ebbd46ec215c9a1a03ab6feb3b
Thomas Boyt <thomas.boyt@venmo.com>
2014-03-31 15:40:35 -0400

    Add AMD-wrapped CJS rewriter

--- src/index.js
+++ src/index.js
@@ -2,19 +2,23 @@ require('../lib/traceur-runtime');
 
 var rewriters = {
   cjs: require('./cjs_rewriter'),
+  amdWrappedCjs: require('./rjs_cjs_rewriter'),
   amd: require('./amd_rewriter'),
   yui: require('./yui_rewriter')
 };
 
-module.exports = {
-  transpile: function(src, type, opts) {
-    var Rewriter = rewriters[type];
-    if ( !Rewriter ) {
-      /* jshint ignore:start */
-      throw new Error(`No transpiler found for type ${type}!`);
-      /* jshint ignore:end */
-    }
-
-    return new Rewriter(src, opts).rewrite();
+function transpile(src, type, opts) {
+  var Rewriter = rewriters[type];
+  if ( !Rewriter ) {
+    /* jshint ignore:start */
+    throw new Error(`No transpiler found for type ${type}!`);
+    /* jshint ignore:end */
   }
+
+  return new Rewriter(src, opts).rewrite();
+}
+
+module.exports = {
+  transpile: transpile,
+  compile: transpile // nicer word!
 };
---
+++ src/rjs_cjs_rewriter.js
@@ -0,0 +1,32 @@
+// This rewriter further wraps the CJS rewriter's output in an AMD module declaration. This should
+// work for RequireJS and Almond, which include RJS compatibility layers.
+
+var recast = require("recast");
+var n = recast.types.namedTypes;
+var b = recast.types.builders;
+
+var CJSRewriter = require('./cjs_rewriter');
+
+// say that five times fast
+class RequireJSCJSRewriter extends CJSRewriter {
+  postRewrite() {
+    // wrap ast.program.body in a function
+    var body = this.ast.program.body;
+    //define(function(require, exports, module) {
+    var amdWrapped = b.expressionStatement(
+      b.callExpression(
+        b.identifier('define'), [
+          b.functionExpression(
+            null,
+            [b.identifier('require'), b.identifier('exports'), b.identifier('module')],
+            b.blockStatement(body)
+          )
+        ]
+      )
+    );
+
+    this.ast.program.body = [amdWrapped];
+  }
+}
+
+module.exports = RequireJSCJSRewriter;
